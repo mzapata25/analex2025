035ee5236e031b5b97d4b7aeb530af2673dde426
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-06-06 04:03:05 -0600

    Update automata_proyecto.pl

--- automata_proyecto.pl
+++ automata_proyecto.pl
@@ -11,5 +11,20 @@ move(c,z,d).
 move(c,h,c).
 move(h,z,a).
 
-accepted(z).
-accepted(c).
+estado_final(z).
+estado_final(c).
+
+revisa_automata(Lista) :-
+    % Uso de función que revisa la lista, el estado inicial es y
+    automata(Lista, y).
+
+% Caso base. La lista está vacía, se compara si el estado actual es uno de los finales.
+automata([], EstadoActual) :-
+    estado_final(EstadoActual).
+
+% Con una lista no vacía
+automata([Letra | Resto], EstadoActual) :-
+    % Comprobación de que el estado actual si tiene conexión con la letra correspondiente
+    move(EstadoActual, Siguiente, Letra),
+    % Cuando si estan conectadas
+    automata(Resto, Siguiente).
