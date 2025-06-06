943a410d1ea972400d583efb7c445cd0a441b668
mzapata25 <142268986+mzapata25@users.noreply.github.com>
2025-06-06 03:19:05 -0600

    Add files via upload

---
+++ automata_proyecto.pl
@@ -0,0 +1,15 @@
+move(y,a,a).
+move(a,b,m).
+move(b,d,i).
+move(d,z,l).
+move(a,c,n).
+move(c,e,a).
+move(e,f,r).
+move(f,g,y).
+move(g,z,a).
+move(c,z,d).
+move(c,h,c).
+move(h,z,a).
+
+accepted(z).
+accepted(c).
